b730a42c3c5ff440f334f91d0aefdcddb294025b
andrasfelfoldi <andras.felfoldi@gmail.com>
2019-08-02 07:16:32 +0200

    some checks to prevent unnecessary requests

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -10,31 +10,49 @@ import { SharedDataService } from "src/app/services/shared-data/shared-data.serv
 })
 export class HomeComponent implements OnInit {
   repos: Repository[] = [];
+  prevSearchTerm: string = "";
 
   constructor(
     private gitHubService: GitHubService,
     private sharedDataService: SharedDataService
-  ) {}
+  ) {
+    this.prevSearchTerm = this.sharedDataService.searchTerm;
+  }
 
   ngOnInit() {
-    // this.repos = this.gitHubService.getRepositoriesByName("asd");
-    this.gitHubService
-      .getRepositoriesByName(this.sharedDataService.searchTerm)
-      .subscribe((data: any) => {
-        data.items.forEach(repo => {
-          this.repos.push(
-            new Repository(
-              repo.name,
-              repo.description,
-              repo.owner.login,
-              repo.html_url,
-              repo.forks_count,
-              repo.stargazers_count,
-              repo.open_issues_count,
-              repo.issues_url
-            )
-          );
+    this.getRepositories();
+  }
+
+  getRepositories() {
+    console.log(this.prevSearchTerm);
+    console.log(this.sharedDataService.searchTerm);
+
+    if (
+      this.prevSearchTerm !== this.sharedDataService.searchTerm ||
+      this.sharedDataService.repos.length === 0
+    ) {
+      this.gitHubService
+        .getRepositoriesByName(this.sharedDataService.searchTerm)
+        .subscribe((data: any) => {
+          data.items.forEach(repo => {
+            this.repos.push(
+              new Repository(
+                repo.name,
+                repo.description,
+                repo.owner.login,
+                repo.html_url,
+                repo.forks_count,
+                repo.stargazers_count,
+                repo.open_issues_count,
+                repo.issues_url
+              )
+            );
+          });
+
+          this.sharedDataService.repos = this.repos;
         });
-      });
+    } else {
+      this.repos = this.sharedDataService.repos;
+    }
   }
 }
--- src/app/screens/issues/issues.component.ts
+++ src/app/screens/issues/issues.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { GitHubService } from "../../services/git-hub/git-hub.service";
 import { Issue } from "src/app/models/Issue";
 import { ActivatedRoute } from "@angular/router";
+import { SharedDataService } from "../../services/shared-data/shared-data.service";
 
 @Component({
   selector: "app-issues",
@@ -15,19 +16,36 @@ export class IssuesComponent implements OnInit {
 
   constructor(
     private gitHubService: GitHubService,
-    private route: ActivatedRoute
-  ) {}
-
-  ngOnInit() {
+    private route: ActivatedRoute,
+    private sharedDataService: SharedDataService
+  ) {
     this.owner = this.route.snapshot.paramMap.get("owner");
     this.repoName = this.route.snapshot.paramMap.get("repoName");
+  }
 
-    this.gitHubService
-      .getIssuesForRepository(this.owner, this.repoName)
-      .subscribe((data: any) => {
-        data.items.forEach(issue => {
-          this.issues.push(issue);
+  ngOnInit() {
+    this.getIssues();
+  }
+
+  getIssues() {
+    if (
+      this.repoName !== this.sharedDataService.repoName ||
+      this.owner !== this.sharedDataService.ownerName ||
+      this.sharedDataService.issues.length === 0
+    ) {
+      this.gitHubService
+        .getIssuesForRepository(this.owner, this.repoName)
+        .subscribe((data: any) => {
+          data.items.forEach(issue => {
+            this.issues.push(issue);
+          });
+
+          this.sharedDataService.issues = this.issues;
+          this.sharedDataService.repoName = this.repoName;
+          this.sharedDataService.ownerName = this.owner;
         });
-      });
+    } else {
+      this.issues = this.sharedDataService.issues;
+    }
   }
 }
