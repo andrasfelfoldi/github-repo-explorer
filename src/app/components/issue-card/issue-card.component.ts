d6fbd3141daa35175cc91ddb14c25f893b98d510
andrasfelfoldi <andras.felfoldi@gmail.com>
2019-08-05 20:36:31 +0200

    added markdown display in issue card

--- src/app/components/issue-card/issue-card.component.ts
+++ src/app/components/issue-card/issue-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Issue } from 'src/app/models/Issue';
+import MarkdownIt from 'markdown-it';
 
 @Component({
   selector: 'app-issue-card',
@@ -13,6 +14,15 @@ export class IssueCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    let markdown = new MarkdownIt();
+    var result = markdown.render(this.issue.body);
+    document.getElementById(`${this.issue.id}`).innerHTML = result;
   }
 
 }
