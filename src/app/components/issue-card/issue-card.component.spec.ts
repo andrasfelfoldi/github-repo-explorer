60836bb4333c03b17e3b234fe2dfa8eb0ee3a914
andrasfelfoldi <andras.felfoldi@gmail.com>
2019-08-07 07:01:30 +0200

    issue-card-component tests now run

--- src/app/components/issue-card/issue-card.component.spec.ts
+++ src/app/components/issue-card/issue-card.component.spec.ts
@@ -1,25 +1,50 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IssueCardComponent } from './issue-card.component';
+import { Component } from '@angular/core';
 
 describe('IssueCardComponent', () => {
-  let component: IssueCardComponent;
-  let fixture: ComponentFixture<IssueCardComponent>;
+  let hostComponent: TestHostComponent;
+  let issueCardComponent: IssueCardComponent;
+  let hostFixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IssueCardComponent ]
+      declarations: [IssueCardComponent, TestHostComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(IssueCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    hostFixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = hostFixture.componentInstance;
+    issueCardComponent = hostFixture.debugElement.children[0].componentInstance;
+    hostFixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(issueCardComponent).toBeTruthy();
   });
 });
+
+const fakeIssue = {
+  id: "123",
+  body: "test_issue_body",
+  state: "open",
+  title: "test_title",
+  html_url: "test_url",
+  user: {
+    html_url: "user_url",
+    login: "user_login"
+  },
+  created_at: "2019-04-22T07:28:35Z"
+}
+
+@Component({
+  selector: `host-component`,
+  template: `<app-issue-card [issue]="issue"></app-issue-card>`
+})
+class TestHostComponent {
+  issue = fakeIssue;
+}
+
